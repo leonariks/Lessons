a5c9022c5dc945718df69035abe17353936fb601
Kirill <89861337+leonariks@users.noreply.github.com>
2023-01-20 16:10:57 +0300

    j

--- JAVA/тема_10/lesson_10/src/Cat.java
+++ JAVA/тема_10/lesson_10/src/Cat.java
@@ -4,7 +4,32 @@ public class Cat {
     int age;
     double weight;
 
+    {
+        name = "Kilimangaro";
+        breed = "Amogus";
+        printCatInfo();
+        System.out.printf("Lol");
+    }
+    Cat(String n, int a) {
+        name = n;
+        age = a;
+        breed = "неизвестно";
+        weight = 0;
+    }
+
+    Cat(){
+
+    }
+    Cat(String name, String breed, int age, double weight) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.weight = weight;
+    }
+
     void printCatInfo() {
-        System.out.printf("Имя кота: %s, порода: %s, возраст: %d, вес: %f", name, breed, age, weight);
+        String name;
+        name = "Oppok";
+        System.out.printf("Имя кота: %s, порода: %s, возраст: %d, вес: %f \n", this.name, breed, age, weight);
     }
 }
--- JAVA/тема_10/lesson_10/src/Main.java
+++ JAVA/тема_10/lesson_10/src/Main.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Cat cat = new Cat();
-        cat.age = 1;
-        cat.name = "Джемик";
-        cat.breed ="Курильский бобтейл";
-        cat.weight = 4.1;
+//        cat.age = 1;
+//        cat.name = "Джемик";
+//        cat.breed = "Курильский бобтейл";
+//        cat.weight = 4.1;
         cat.printCatInfo();
+
+        Cat djemik = new Cat("Джемик",1);
+        djemik.printCatInfo();
+        Cat barsik = new Cat("Джемик", "Курбобик", 1, 4.1);
+        barsik.printCatInfo();
         //System.out.println("Имя : " + cat.name + "\n" + "Возраст : " + cat.age);
     }
 }
