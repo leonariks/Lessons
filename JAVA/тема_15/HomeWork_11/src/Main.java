b4ab9f79bae3557da4e5232605e9da3df2c6f733
Kirill <89861337+leonariks@users.noreply.github.com>
2023-02-12 19:00:18 +0300

    1

---
+++ JAVA/тема_15/HomeWork_11/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+       TreeSet<Integer> integers = new TreeSet<>();
+       Scanner in = new Scanner(System.in);
+       int a = in.nextInt();
+
+    }
+}
