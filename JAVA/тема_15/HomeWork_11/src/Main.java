77371ba006e75d09602be4d45592e3362dbfc8b9
Kirill <89861337+leonariks@users.noreply.github.com>
2023-02-19 13:11:36 +0300

    homework

--- JAVA/тема_15/HomeWork_11/src/Main.java
+++ JAVA/тема_15/HomeWork_11/src/Main.java
@@ -3,9 +3,25 @@ import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-       TreeSet<Integer> integers = new TreeSet<>();
+       TreeSet<Integer> collection = new TreeSet<>();
+        System.out.println("Введите значения:");
        Scanner in = new Scanner(System.in);
-       int a = in.nextInt();
-
+       while(true){
+           if(!collection.add(in.nextInt())){
+               for(Integer value:collection){
+                   System.out.println(value);
+               }
+               System.out.println(collection.size());
+               System.out.println("Введите число для удаления");
+               if (collection.remove(in.nextInt())){
+                   for(Integer value:collection){
+                       System.out.println(value);
+                   }
+               }else {
+                   System.out.println("Удаление нее было завершено");
+               }
+               return;
+           }
+       }
     }
 }
