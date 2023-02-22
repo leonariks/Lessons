43c56274efa6ea27e4ebf267f231d6501323990f
Kirill <89861337+leonariks@users.noreply.github.com>
2023-02-22 22:29:28 +0300

    Pract

---
+++ JAVA/тема_16/homework_12/HomeWork_12/src/User.java
@@ -0,0 +1,11 @@
+public class User {
+    private String secondName;
+    private String firstName;
+    private String surname;
+    private int telephone;
+    private String email;
+    private String login;
+    private String pas;
+
+
+}
