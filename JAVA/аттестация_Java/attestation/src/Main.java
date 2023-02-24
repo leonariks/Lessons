f3933ad7a488b8b75ffdeaec74b7fd3291142c44
Kirill <89861337+leonariks@users.noreply.github.com>
2023-02-25 00:31:07 +0300

    not

--- JAVA/аттестация_Java/attestation/src/Main.java
+++ JAVA/аттестация_Java/attestation/src/Main.java
@@ -1,5 +1,107 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        auth();
+    }
 
+    public static void auth() {
+        Scanner in = new Scanner(System.in);
+        ArrayList<User> users = new ArrayList<>();
+        User admin = new User();
+        admin.setLogin("admin");
+        admin.setPass("pass");
+        while (true) {
+            User user = new User();
+            System.out.println("1 - Регистрация\n2 - Авторизация");
+            int input = in.nextInt();
+            in.nextLine();
+            switch (input) {
+                case 1:
+
+                    boolean nonunic;
+                    do {
+                        nonunic = false;
+                        System.out.println("Login:");
+                        user.setLogin(in.nextLine());
+                        for (User us : users) {
+                            if (us.equals(user)) {
+                                nonunic = true;
+                                System.out.println("Логин занят!");
+                            }
+                        }
+                    } while (nonunic);
+                    System.out.println("Password:");
+                    user.setPass(in.nextLine());
+                    System.out.println("First Name:");
+                    user.setFirstName(in.nextLine());
+                    System.out.println("Second Name:");
+                    user.setSecondName(in.nextLine());
+                    System.out.println("Surname:");
+                    user.setSurname(in.nextLine());
+                    do {
+                        nonunic = false;
+                        System.out.println("Telephone:");
+                        while (!user.setTel(in.nextLine())) ;
+                        for (User us : users) {
+                            if (us.equals(user)) {
+                                nonunic = true;
+                                System.out.println("Телефон занят!");
+                            }
+                        }
+                    } while (nonunic);
+                    do {
+                        nonunic = false;
+                        System.out.println("Email:");
+                        while (!user.setEmail(in.nextLine())) ;
+                        for (User us : users) {
+                            if (us.equals(user)) {
+                                nonunic = true;
+                                System.out.println("Телефон занят!");
+                            }
+                        }
+                    } while (nonunic);
+                    users.add(user);
+                    System.out.println("Аккаунт успешно зарегистрирован!");
+                    break;
+
+                case 2:
+
+                    System.out.println("Login:");
+                    user.setLogin(in.nextLine());
+                    System.out.println("Password:");
+                    user.setPass(in.nextLine());
+                    if (!findAdmin(user.getLogin(), user.getPass(), users)){
+                        System.out.println("Неккоректный Логин/Пароль.");
+                    }else if (!findUser(user.getLogin(), user.getPass(), users)) {
+                        System.out.println("Неккоректный Логин/Пароль.");
+                    }
+                    break;
+
+                default:
+                    System.out.println("Non");
+            }
+        }
+    }
+
+    static boolean findAdmin(String login, String pass, ArrayList<User> users) {
+        for (User admin : users) {
+            if (admin.getLogin().equals(login) && admin.getPass().equals(pass)) {
+                System.out.println("Вы авторизованы в роли администратора!");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean findUser(String login, String pass, ArrayList<User> users) {
+        for (User user : users) {
+            if (user.getLogin().equals(login) && user.getPass().equals(pass)) {
+                System.out.println("Вы авторизованы!");
+                return true;
+            }
+        }
+        return false;
     }
 }
--- JAVA/аттестация_Java/attestation/src/User.java
+++ JAVA/аттестация_Java/attestation/src/User.java
@@ -1,2 +1,102 @@
+import java.util.Objects;
+
 public class User {
+
+    private String login;
+    private String pass;
+    private String firstName;
+    private String secondName;
+    private String surname;
+    private String tel;
+    private String email;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public boolean setTel(String tel) {
+        if (tel.matches("(\\+7|7|8)+(\\d{10})")) {
+            this.tel = tel;
+            return true;
+        } else {
+            System.out.println("Неккоректный номер телефона");
+            return false;
+        }
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean setEmail(String email) {
+        if (email.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"+"[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")) {
+            this.email = email;
+            return true;
+        } else {
+            System.out.println("Неккоректный email");
+            return false;
+        }
+
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!Objects.equals(login, user.login)) return false;
+        if (!Objects.equals(tel, user.tel)) return false;
+        return Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = login != null ? login.hashCode() : 0;
+        result = 31 * result + (tel != null ? tel.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
 }
